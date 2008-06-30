469ab98d9ac302c279ca2ef80b08587a3087c0b3
onigame <onigame@79bcd662-254d-0410-aa26-a7f5a6b4d6aa>
2008-06-30 22:08:06 +0000

    Added a way to delete log lines.

--- logger.py
+++ logger.py
@@ -14,13 +14,17 @@ def LogOneEntry(content):
   return entry
 
 # gets the last "num" log lines.
-# we do this by having a window that increases exponentially
-# until it is large enough (or too large).
 def GetLastLogs(num):
   query = LogEntry.all()
   query.order("-datetime")
   results = query.fetch(num)
-  results.reverse()
+  return results
+
+# gets the first "num" log lines.
+def GetFirstLogs(num):
+  query = LogEntry.all()
+  query.order("datetime")
+  results = query.fetch(num)
   return results
 
 class LogEntry(db.Model):
@@ -46,8 +50,31 @@ class LogReader(webapp.RequestHandler):
 class LogReaderLast(webapp.RequestHandler):
   def get(self):
     entries = GetLastLogs(int(self.request.get('count')))
+    entries.reverse()
     self.response.headers['Content-Type'] = 'text/plain'
     for s in entries:
       if s:
         self.response.out.write("%s %s\n" % (s.datetime.isoformat(' '), s.content))
+
+class LogReaderFirst(webapp.RequestHandler):
+  def get(self):
+    entries = GetFirstLogs(int(self.request.get('count')))
+    self.response.headers['Content-Type'] = 'text/html'
+    
+    self.response.out.write("<pre>");
+    for s in entries:
+      if s:
+        self.response.out.write("%s %s\n" % (s.datetime.isoformat(' '), s.content))
+    self.response.out.write("</pre>");
+    self.response.out.write("<a href='/datastore/message-delete-first?count=%s'>Nuke them all</a>" % int(self.request.get('count')))
+
+class LogDeleterFirst(webapp.RequestHandler):
+  def get(self):
+    entries = GetFirstLogs(int(self.request.get('count')))
+    self.response.headers['Content-Type'] = 'text/plain'
+    for s in entries:
+      if s:
+        self.response.out.write("%s %s\n" % (s.datetime.isoformat(' '), s.content))
+        db.delete(s)
+    self.response.out.write("Deleted.")
 
